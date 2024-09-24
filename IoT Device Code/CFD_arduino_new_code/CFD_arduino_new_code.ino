4019431bde152a6102582f910c56c906f2fb1d35
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2024-09-24 07:27:55 +0600

    Revamped arduino code
    Major overhaul of the algorithm.
    Averages all data for 5 seconds before sending.

--- IoT Device Code/CFD_arduino_new_code/CFD_arduino_new_code.ino
+++ IoT Device Code/CFD_arduino_new_code/CFD_arduino_new_code.ino
@@ -5,105 +5,121 @@
 #include "MHZ19.h"
 
 #define SEA_LEVEL_PRESSURE 1013.0f
-float temp;
-float hum;
-float pressure;
-float alt;
-float pm1;
-float pm25;
-float pm10;
 
+// Global variables for sensor data
+float temp, hum, pressure, alt, pm1, pm25, pm10, CO2;
+float tempAvg, humAvg, pressureAvg, altAvg, pm1Avg, pm25Avg, pm10Avg, CO2Avg;
 String str = "";
 
+// Sensor objects
 DFRobot_BME280_IIC bme(&Wire, 0x77);
-
 PMS pms(Serial2);
-PMS::DATA data; 
-
-MHZ19 myMHZ19;                                             // Constructor for library
+PMS::DATA data;
+MHZ19 myMHZ19;
 unsigned long getDataTimer = 0;
 SHT35 sht35(21);  // Assuming SCL is connected to pin 21
 
 void setup() {
-  Serial.begin(9600);
-  sht35.init();
-  //MH-Z19B
-  Serial1.begin(9600);  //MH-Z19B Serial
-  myMHZ19.begin(Serial1);                                // *Serial(Stream) refence must be passed to library begin().
-  myMHZ19.autoCalibration(false);                              // Turn auto calibration ON (OFF autoCalibration(false))
-  //PMS5003
-  Serial2.begin(9600);  //PMS5003 Serial
-  pms.passiveMode();    // Switch to passive mode
-  pms.wakeUp();
-  //BME280
-  while (bme.begin() != DFRobot_BME280_IIC::eStatusOK) {
-        Serial.println("bme begin failed");
+    Serial.begin(9600);
+
+    // Initialize SHT35
+    sht35.init();
+
+    // Initialize MH-Z19B
+    Serial1.begin(9600);  // MH-Z19B Serial
+    myMHZ19.begin(Serial1);  // Pass Serial to library
+    myMHZ19.autoCalibration(false);  // Disable auto calibration
+
+    // Initialize PMS5003
+    Serial2.begin(9600);  // PMS5003 Serial
+    pms.passiveMode();  // Switch to passive mode
+    pms.wakeUp();
+
+    // Initialize BME280
+    while (bme.begin() != DFRobot_BME280_IIC::eStatusOK) {
+        Serial.println("BME280 initialization failed");
         delay(2000);
     }
-  Serial3.begin(115200);
+
+    // Initialize Serial3
+    Serial3.begin(115200);
 }
 
 void loop() {
-  sht35.read_meas_data_single_shot(HIGH_REP_WITH_STRCH, &temp, &hum);
-  pressure = bme.getPressure();
-  alt = bme.calAltitude(SEA_LEVEL_PRESSURE, pressure)/100;
+    // Static variables to store accumulated values and counts
+    static float tempSum = 0, humSum = 0, pressureSum = 0, altSum = 0;
+    static float pm1Sum = 0, pm25Sum = 0, pm10Sum = 0, CO2Sum = 0;
+    static int sampleCount = 0;
+    static unsigned long startTime = millis();  // Start time for 5-second interval
 
-  //PMS5003 Readings
-  //pms.wakeUp();
-  //delay(2000);
-  pms.requestRead();
-  if (pms.readUntil(data))
-  {
-    pm1 = data.PM_AE_UG_1_0;
-    pm25 = data.PM_AE_UG_2_5;
-    pm10 = data.PM_AE_UG_10_0;
-  }
-  //pms.sleep();
+    readSensors();  // Read sensor data
 
-  static int CO2=0;
-  int8_t temp2;
-  if (millis() - getDataTimer >= 2000){
-        CO2 = myMHZ19.getCO2();  // Request CO2 (as ppm)                                
-        getDataTimer = millis();
-   }
+    // Accumulate values
+    tempSum += temp;
+    humSum += hum;
+    pressureSum += pressure;
+    altSum += alt;
+    pm1Sum += pm1;
+    pm25Sum += pm25;
+    pm10Sum += pm10;
+    CO2Sum += CO2;  // Fixed accumulation of CO2
 
-  //Serial Printing Data
-  Serial.println("------xxxxxxxxxxxxxxxxxxxxxxxx--------");
-  Serial.print("Temp: ");
-  Serial.println(temp);
-  Serial.print("Humi: ");
-  Serial.println(hum);
-  Serial.print("Air Pressure: ");
-  Serial.println(pressure);
-  Serial.print("Altitude: ");
-  Serial.println(alt);
-  Serial.print("PM1.0: ");
-  Serial.println(pm1);
-  Serial.print("PM2.5: ");
-  Serial.println(pm25);
-  Serial.print("PM10.0: ");
-  Serial.println(pm10);
-  Serial.print("CO2: ");
-  Serial.println(CO2);
+    sampleCount++;
 
-  str = "";
-  str += temp;
-  str += ";";
-  str += hum;
-  str += ";";
-  str += pressure;
-  str += ";";
-  str += alt;
-  str += ";";
-  str += pm1;
-  str += ";";
-  str += pm25;
-  str += ";";
-  str += pm10;
-  str += ";";
-  str += CO2;
+    // Check if 5 seconds have passed
+    if (millis() - startTime >= 5000) {
+        // Calculate averages
+        tempAvg = tempSum / sampleCount;
+        humAvg = humSum / sampleCount;
+        pressureAvg = pressureSum / sampleCount;
+        altAvg = altSum / sampleCount;
+        pm1Avg = pm1Sum / sampleCount;
+        pm25Avg = pm25Sum / sampleCount;
+        pm10Avg = pm10Sum / sampleCount;
+        CO2Avg = CO2Sum / sampleCount;  // Calculate CO2 average
+
+        // Reset accumulators and counter
+        tempSum = humSum = pressureSum = altSum = 0;
+        pm1Sum = pm25Sum = pm10Sum = CO2Sum = 0;
+        sampleCount = 0;
+        startTime = millis();  // Reset the timer
+
+        sendData();  // Send averaged data
+    }
+}
+
+void readSensors() {
+    // Read SHT35
+    sht35.read_meas_data_single_shot(HIGH_REP_WITH_STRCH, &temp, &hum);
+    pressure = bme.getPressure();
+    alt = bme.calAltitude(SEA_LEVEL_PRESSURE, pressure) / 100;
+
+    // Read PM sensor data
+    pms.requestRead();
+    if (pms.readUntil(data)) {
+        pm1 = data.PM_AE_UG_1_0;
+        pm25 = data.PM_AE_UG_2_5;
+        pm10 = data.PM_AE_UG_10_0;
+    }
+
+    CO2 = myMHZ19.getCO2();  // Request CO2 (as ppm)
+}
+
+void sendData() {
+  // Send averaged data over Serial3
+  str = String(tempAvg) + ";" + String(humAvg) + ";" + String(pressureAvg) + ";" + String(altAvg) + ";" +
+        String(pm1Avg) + ";" + String(pm25Avg) + ";" + String(pm10Avg) + ";" + String(CO2Avg);
 
   Serial3.println(str);
   
-  delay(4500);
+  Serial.println("------xxxxxxxxxxxxxxxxxxxxxxxx--------");
+
+  Serial.println("Temp: " + String(temp));
+  Serial.println("Humi: " + String(hum));
+  Serial.println("Air Pressure: " + String(pressure));
+  Serial.println("Altitude: " + String(alt));
+  Serial.println("PM1.0: " + String(pm1));
+  Serial.println("PM2.5: " + String(pm25));
+  Serial.println("PM10.0: " + String(pm10));
+  Serial.println("CO2: " + String(CO2));
 }
