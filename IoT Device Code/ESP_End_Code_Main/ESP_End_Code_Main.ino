607392f3483a190259fca1f2a82f186efe14f418
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2024-09-24 06:42:32 +0600

    New ESP Code

--- IoT Device Code/ESP_End_Code_Main/ESP_End_Code_Main.ino
+++ IoT Device Code/ESP_End_Code_Main/ESP_End_Code_Main.ino
@@ -1,23 +1,20 @@
 #include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <WebSerialLite.h>
 #include <PubSubClient.h>
 
 // WiFi credentials
 const char* ssid = "Mahir 2.4GHz";
 const char* password = "01741238814";
+ 
 
 // MQTT broker settings
-const char* mqtt_server = "103.237.39.27"; 
+const char* mqtt_server = "103.237.39.27";
 const char* mqtt_topic = "esp8266/sensorData";  // Topic to publish data to
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-AsyncWebServer server(80);
 
 // Sensor data (dummy data)
-int id = 1;
+int id = 0;
 float temp = 0;
 float hum = 0;
 float Pressure = 0;
@@ -39,10 +36,8 @@ void recvMsg(uint8_t *data, size_t len) {
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   setup_wifi();
-  WebSerial.begin(&server);
-  server.begin();
 
   client.setServer(mqtt_server, 1883);  // Set the MQTT broker and port
 }
@@ -51,15 +46,15 @@ void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  WebSerial.println("New loop");
+  else {
+    Serial.println("UWU");
+    }
   client.loop();
 
   // Check for serial input
-  
   readSerialData();
   
-  
-  //delay(5000);  // Delay for testing, adjust as needed
+  delay(1000);  // Delay for testing, adjust as needed
 }
 
 void setup_wifi() {
@@ -68,6 +63,11 @@ void setup_wifi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
+  Serial.print("Successfully connected to : ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 }
 
 void reconnect() {
@@ -113,17 +113,16 @@ void parseDataString(String inputString) {
 
 // Publish data to MQTT broker
 void sendData(int id, float tem, float hum, float pressure, float alt, float pm1, float pm25, float pm10, float co2) {
-  WebSerial.print("sendData function called");
   // Create a JSON-like string payload
   String payload = "{\"id\":" + String(id) + ",\"air_temperature\":" + String(tem) + ",\"humidity\":" + String(hum) +
                    ",\"pressure\":" + String(pressure) + ",\"pm1\":" + String(pm1) + ",\"pm2_5\":" + String(pm25) + 
                    ",\"pm10\":" + String(pm10) + ",\"co2\":" + String(co2) + "}";
 
   // Publish the payload to the topic
-  WebSerial.println(payload.c_str());
   if (client.publish(mqtt_topic, payload.c_str())) {
     // Publish successful
-    WebSerial.println("Publish successful");
+    Serial.println("Data Published:");
+    Serial.println(payload);
   }
 }
 
@@ -131,7 +130,6 @@ void sendData(int id, float tem, float hum, float pressure, float alt, float pm1
 void readSerialData() {
   // Check if there's serial data available
   if (Serial.available()) {
-    WebSerial.print("Receiving data from Arduino");
     lastSerialReadTime = millis();  // Reset the timeout timer
 
     while (Serial.available()) {
@@ -139,6 +137,8 @@ void readSerialData() {
       if (incomingChar == '\n') {  // Newline indicates end of message
         parseDataString(msg);  // Parse the complete message
         sendData(id, temp, hum, Pressure, alt, PM1, PM25, PM10, CO2);  // Send parsed data via MQTT
+        Serial.println("Device Data:");
+        Serial.println(msg);
         msg = "";  // Clear the message buffer
       } else {
         msg += incomingChar;  // Append the incoming character to the message
@@ -150,17 +150,4 @@ void readSerialData() {
   if (millis() - lastSerialReadTime > serialTimeout && msg.length() > 0) {
     msg = "";  // Reset the buffer if incomplete data has been sitting for too long
   }
-}
-
-// This function is called when data is received from the browser
-void onWebSerialReceive(uint8_t *data, size_t len) {
-  String receivedText = "";
-  for (int i = 0; i < len; i++) {
-    receivedText += char(data[i]);
-  }
-  Serial.println("Received via WebSerial: " + receivedText);
-  
-  // Echo back the received message to the browser
-  WebSerial.print("Echo: ");
-  WebSerial.println(receivedText);
 }
