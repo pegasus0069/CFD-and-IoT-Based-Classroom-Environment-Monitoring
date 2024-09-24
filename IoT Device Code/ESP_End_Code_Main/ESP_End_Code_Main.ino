a291a7aa5fb8cff166a445831d3a99c90eccc0e0
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2024-09-24 11:38:39 +0600

    ESP8266 OTA Update

--- IoT Device Code/ESP_End_Code_Main/ESP_End_Code_Main.ino
+++ IoT Device Code/ESP_End_Code_Main/ESP_End_Code_Main.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoOTA.h>  // Include the OTA library
 
 // WiFi credentials
 const char* ssid = "Mahir 2.4GHz";
@@ -32,6 +33,10 @@ void setup() {
     setup_wifi();
 
     client.setServer(mqtt_server, 1883);  // Set the MQTT broker and port
+
+    // Initialize OTA
+    ArduinoOTA.setPassword("espota");
+    setupOTA();
 }
 
 void loop() {
@@ -41,6 +46,9 @@ void loop() {
         Serial.println("Connected to MQTT broker.");
     }
     client.loop();
+
+    // Handle OTA updates
+    ArduinoOTA.handle();
 
     // Check for serial input
     readSerialData();
@@ -57,6 +65,41 @@ void setup_wifi() {
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
     Serial.println();
+}
+
+void setupOTA() {
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+        } else {  // U_SPIFFS
+            type = "filesystem";
+        }
+        Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
+    });
+
+    ArduinoOTA.begin();
+    Serial.println("OTA ready");
 }
 
 void reconnect() {
