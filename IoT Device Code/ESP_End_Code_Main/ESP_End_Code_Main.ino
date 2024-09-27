522977258954aac00ef6bd0e4476eb9f8a270756
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2024-09-27 21:51:00 +0600

    Update ESP_End_Code_Main.ino

--- IoT Device Code/ESP_End_Code_Main/ESP_End_Code_Main.ino
+++ IoT Device Code/ESP_End_Code_Main/ESP_End_Code_Main.ino
@@ -6,30 +6,25 @@
 //Time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 6*3600, 60000); // UTC offset 0
-
-// WiFi credentials
+// WiFi
 const char* ssid = "Mahir 2.4GHz";
 const char* password = "01741238814";
-
+WiFiClient espClient;
 // MQTT broker settings
 const char* mqtt_server = "103.237.39.27";
 const char* mqtt_topic = "esp8266/sensorData";  // Topic to publish data to
-
-WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
+//Data
 String timestamp = "";
-
 String incomingData = "";  // Store incoming serial data
 
 void setup() {
     Serial.begin(115200);
-
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
     }
-
     mqttClient.setServer(mqtt_server, 1883);  // Set the MQTT broker and port
 }
 
@@ -39,22 +34,18 @@ void loop() {
           mqttClient.connect("ESP8266Client");
         }
     }
-
     mqttClient.loop(); //Required to keep MQTT Connection
     updateTime();
 
-    // Check for serial input
+    //Read Serial
     if (Serial.available() > 0) {
       incomingData = Serial.readStringUntil('\n');
     }
-
-}
-
-// Publish data to MQTT broker
-void sendMQTTMessage() {
+    //Send data to server
     if (mqttClient.connected()) {
-        mqttClient.publish(mqtt_topic, incomingData.c_str()); //Data not guaranteed to be sent
+        mqttClient.publish(mqtt_topic, incomingData.c_str());
     }
+
 }
 
 void updateTime() {
@@ -62,10 +53,8 @@ void updateTime() {
     time_t rawTime = timeClient.getEpochTime();
     struct tm * timeinfo;
     timeinfo = localtime(&rawTime);
-    
     char buffer[20]; // 2024-09-24 09:52:31
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
-    
     timestamp = buffer;
 }
 
